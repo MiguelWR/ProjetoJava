54961acc67ad1befe48051881cdd9f894fece67a
João Vitor D'El Rey <jvitordelreyraposo123@gmail.com>
2024-10-31 09:50:24 -0300

    Atualização no código

--- src/Chevette.java
+++ src/Chevette.java
@@ -1,4 +1,4 @@
-public class Chevette extends Carro{
+public class Chevette extends Carro {
     public Chevette(int portas, String cor, int cavalos, int ano, int cilindros, double valor) {
         super(portas, cor, cavalos, ano, cilindros, valor);
     }
