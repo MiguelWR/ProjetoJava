03a27836f58f8bb50080061e4da34b5d55838ffb
MiguelWR <115580465+MiguelWR@users.noreply.github.com>
2024-10-31 11:13:47 -0300

    Update Cliente.java

--- src/Cliente.java
+++ src/Cliente.java
@@ -10,4 +10,30 @@ public class Cliente {
         this.saldo = saldo;
         this.cpf = cpf;
     }
+//GETs e SETs Cliente
+
+    public String getNome(){
+        return nome;
+    }
+    public int getIdade(){
+        return idade;
+    }
+    public double getSaldo(){
+        return saldo;
+    }
+    public String getCpf(){
+        return cpf;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
 }
