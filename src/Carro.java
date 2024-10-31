1dd3745adc0b290349dbb77776b4719153f8ad36
Leander-Antonio <Leander.hallu@gmail.com>
2024-10-31 11:15:51 -0300

    Atualização carro e main
    Atualização carro e main

--- src/Carro.java
+++ src/Carro.java
@@ -14,4 +14,46 @@ public class Carro {
         this.cilindros = cilindros;
         this.valor = valor;
     }
+
+
+    //GETs e SETs
+
+    public int getPortas(){
+        return portas;
+    }
+    public String getCor(){
+        return cor;
+    }
+    public int getCavalos(){
+        return cavalos;
+    }
+    public int getAno(){
+        return ano;
+    }
+    public int getCilindros(){
+        return cilindros;
+    }
+    public double getValor(){
+        return ano;
+    }
+
+
+    public void setPortas(int portas){
+        this.portas = portas;
+    }
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+    public void setCavalos(int cavalos){
+        this.cavalos = cavalos;
+    }
+    public void setAno(int ano){
+        this.ano = ano;
+    }
+    public void setCilindros(int cilindros){
+        this.cilindros = cilindros;
+    }
+    public void setValor(double valor){
+        this.valor = valor;
+    }
 }
