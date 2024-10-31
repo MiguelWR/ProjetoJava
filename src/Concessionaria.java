a283a9c3d95f2c94885b5bb6e543a8ae5a361b83
MiguelWR <115580465+MiguelWR@users.noreply.github.com>
2024-10-31 11:12:57 -0300

    Update Concessionaria.java

--- src/Concessionaria.java
+++ src/Concessionaria.java
@@ -10,5 +10,31 @@ public class Concessionaria {
         this.cnpj = cnpj;
         this.estoque = estoque;
     }
+//GETs e SETs 
+    
+public String getNome(){
+        return nome;
+    }
+    public double getLucro(){
+        return lucro;
+    }
+    public String getCnpj(){
+        return cnpj;
+    }
+    public int getEstoque(){
+        return estoque;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setLucro(double Lucro){
+        this.lucro = lucro;
+    }
+    public void setCnpj(String cnpj){
+        this.cnpj = cnpj;
+    }
+    public void setEstoque(int estoque){
+        this.estoque = estoque;
+    }
 }
 
