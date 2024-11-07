94e8069f7185b5b284e45e52dbb7d71d53cbdc16
MiguelWR <115580465+MiguelWR@users.noreply.github.com>
2024-11-06 22:27:40 -0300

    Update Sandero.java

--- src/Sandero.java
+++ src/Sandero.java
@@ -2,13 +2,13 @@ public class Sandero extends Carro{
     public Sandero(String cor, int ano, double valor) {
         super(4, cor, 110, ano, 3, valor);
     }
+
     @Override
     public void exibirDetalhes() {
-        System.out.println("Sandero: " + cor + ", ano " + ano);
+        System.out.println("Sandero: " + getCor() + ", ano " + getAno());
     }
 
     @Override
-    public void fichaTecnica() {
-        System.out.println("Sandero: " + cor + ", " + portas + " portas, " + cavalos + " cavalos, ano " + ano + ", cilindros: " + cilindros + ", valor: R$" + valor);
+    public String fichaTecnica() {
+        return "Sandero: " + getCor() + ", " + getPortas() + " portas, " + getCavalos() + " cavalos, ano " + getAno() + ", cilindros: " + getCilindros() + ", valor: R$" + getValor();    }
     }
-}
