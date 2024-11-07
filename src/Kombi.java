a70c38f5c1b37232e48330d51ff51fb1426bed50
MiguelWR <115580465+MiguelWR@users.noreply.github.com>
2024-11-06 22:26:30 -0300

    Update Kombi.java

--- src/Kombi.java
+++ src/Kombi.java
@@ -2,13 +2,14 @@ public class Kombi extends Carro{
     public Kombi(String cor, int ano, double valor) {
         super(3, cor, 78, ano, 4, valor);
     }
+
     @Override
     public void exibirDetalhes() {
-        System.out.println("Kombi: " + cor + ", ano " + ano);
+        System.out.println("Kombi: " + getCor() + ", ano " + getAno());
     }
 
     @Override
-    public void fichaTecnica() {
-        System.out.println("Kombi: " + cor + ", " + portas + " portas, " + cavalos + " cavalos, ano " + ano + ", cilindros: " + cilindros + ", valor: R$" + valor);
-    }
+    public String fichaTecnica() {
+        return "Kombi: " + getCor() + ", " + getPortas() + " portas, " + getCavalos() + " cavalos, ano " + getAno() + ", cilindros: " + getCilindros() + ", valor: R$" + getValor();    }
 }
+
