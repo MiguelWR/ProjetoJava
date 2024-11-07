559f22d650c2223c8f11b49851a85574cc274ac5
João Vitor D'El Rey <jvitordelreyraposo123@gmail.com>
2024-11-06 21:00:41 -0300

    implementação de metodos

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Cliente cliente = new Cliente("Ana", 30, 20000, "123.456.789-00");
         Concessionaria concessionaria = new Concessionaria("AutoLux", 500000, "12.345.678/0001-99", 20);
+        ArrayList<Carro> carros = new ArrayList<>();
 
         Carro fusca = new Fusca("Azul", 1970, 12000);
         Carro chevette = new Chevette("Bege", 1978, 22000);
@@ -12,22 +15,20 @@ public class Main {
         Carro kwid = new Kwid("Laranja", 2023, 80000);
         Carro sandero = new Sandero("Prata", 2012, 33000);
 
-        concessionaria.adicionarCarro(fusca);
-        concessionaria.adicionarCarro(chevette);
-        concessionaria.adicionarCarro(celta);
-        concessionaria.adicionarCarro(focus);
-        concessionaria.adicionarCarro(hb20);
-        concessionaria.adicionarCarro(kombi);
-        concessionaria.adicionarCarro(kwid);
-        concessionaria.adicionarCarro(sandero);
+        carros.add(fusca);
+        carros.add(chevette);
+        carros.add(celta);
+        carros.add(focus);
+        carros.add(hb20);
+        carros.add(kombi);
+        carros.add(kwid);
+        carros.add(sandero);
 
-        System.out.println("Carros disponíveis na concessionária " + concessionaria.getNome() + ":");
-        for (Carro carro : concessionaria.getCarros()) {
+        for (Carro carro : carros) {
             carro.exibirDetalhes();
         }
-        System.out.println("================================================================");
-        System.out.println("Configurações dos carros " + concessionaria.getNome() + ":");
-        for (Carro carro : concessionaria.getCarros()) {
+        System.out.println("=============================================================");
+        for (Carro carro : carros) {
             carro.fichaTecnica();
         }
     }
