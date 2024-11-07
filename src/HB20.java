c34c6f50ff01916c679a3f7d2ad2386dc2431c93
MiguelWR <115580465+MiguelWR@users.noreply.github.com>
2024-11-06 22:26:13 -0300

    Update HB20.java

--- src/HB20.java
+++ src/HB20.java
@@ -2,13 +2,13 @@ public class HB20 extends Carro{
     public HB20(String cor, int ano, double valor) {
         super(4, cor, 80, ano, 3, valor);
     }
+
     @Override
     public void exibirDetalhes() {
-        System.out.println("HB20: " + cor + ", ano " + ano);
+        System.out.println("HB20: " + getCor() + ", ano " + getAno());
     }
 
     @Override
-    public void fichaTecnica() {
-        System.out.println("HB20: " + cor + ", " + portas + " portas, " + cavalos + " cavalos, ano " + ano + ", cilindros: " + cilindros + ", valor: R$" + valor);
-    }
+    public String fichaTecnica() {
+        return "HB20: " + getCor() + ", " + getPortas() + " portas, " + getCavalos() + " cavalos, ano " + getAno() + ", cilindros: " + getCilindros() + ", valor: R$" + getValor();    }
 }
