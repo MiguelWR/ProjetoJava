7bec355321539fe31be66cccb2606b8f880b20ed
MiguelWR <115580465+MiguelWR@users.noreply.github.com>
2024-11-06 22:25:46 -0300

    Update Focus.java

--- src/Focus.java
+++ src/Focus.java
@@ -2,12 +2,14 @@ public class Focus extends Carro{
     public Focus(String cor, int ano, double valor) {
         super(4, cor, 175, ano, 3, valor);
     }
+
+    @Override
     public void exibirDetalhes() {
-        System.out.println("Focus: " + cor + ", ano " + ano);
+        System.out.println("Focus: " + getCor() + ", ano " + getAno());
     }
 
     @Override
-    public void fichaTecnica() {
-        System.out.println("Focus: " + cor + ", " + portas + " portas, " + cavalos + " cavalos, ano " + ano + ", cilindros: " + cilindros + ", valor: R$" + valor);
+    public String fichaTecnica() {
+        return "Focus: " + getCor() + ", " + getPortas() + " portas, " + getCavalos() + " cavalos, ano " + getAno() + ", cilindros: " + getCilindros() + ", valor: R$" + getValor();
     }
 }
