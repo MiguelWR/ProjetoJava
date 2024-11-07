14e3a79e1664ed08ed9c3c3aa77fb181085cb14c
MiguelWR <115580465+MiguelWR@users.noreply.github.com>
2024-11-06 22:24:56 -0300

    Update Chevette.java

--- src/Chevette.java
+++ src/Chevette.java
@@ -2,13 +2,14 @@ public class Chevette extends Carro {
     public Chevette(String cor, int ano, double valor) {
         super(2, cor, 60, ano, 4, valor);
     }
+
     @Override
     public void exibirDetalhes() {
-        System.out.println("Chevette: " + cor + ", ano " + ano);
+        System.out.println("Chevette: " + getCor() + ", ano " + getAno());
     }
 
     @Override
-    public void fichaTecnica() {
-        System.out.println("Chevette: " + cor + ", " + portas + " portas, " + cavalos + " cavalos, ano " + ano + ", cilindros: " + cilindros + ", valor: R$" + valor);
+    public String fichaTecnica() {
+        return "Chevette: " + getCor() + ", " + getPortas() + " portas, " + getCavalos() + " cavalos, ano " + getAno() + ", cilindros: " + getCilindros() + ", valor: R$" + getValor();
     }
 }
