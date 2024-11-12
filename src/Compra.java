2a89c8d33b2254b838c7322ac64a578609c123ca
Leander-Antonio <Leander.hallu@gmail.com>
2024-11-12 18:58:18 -0300

    removendo métodos
    removendo métodos não utilizados

--- src/Compra.java
+++ src/Compra.java
@@ -1,5 +1,3 @@
 public abstract class Compra {
     public abstract void transferencia();
-    public abstract void debito();
-    public abstract void situacao();
 }
--- src/CompraCartao.java
+++ src/CompraCartao.java
@@ -3,10 +3,4 @@ public class CompraCartao extends Compra {
     public void transferencia() {
         System.out.println("\nPagamento realizado por transferência bancária.");
     }
-    @Override
-    public void debito() {
-        System.out.println("Pagamento realizado no débito.");
-    }
-    @Override
-    public void situacao() { System.out.println("Compra aprovada."); }
 }
